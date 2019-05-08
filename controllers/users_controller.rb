ea7f7b9dea88f701acdf4544f2e240b2be51d983
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-05-08 09:55:38 +0100

    users controller formatted

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -27,234 +27,149 @@ class UsersController < Sinatra::Base
 
   end
 
+  get "/users/:id" do
 
+    id = params[:id].to_i
+    @user = Users.find(id)
+    @cohorts = Cohorts.all
+    @roles = Roles.all
 
+    erb :'users/show'
 
+  end
 
+  get "/users/:id/edit" do
 
+    id = params[:id].to_i
+    @user = Users.find(id)
 
+    erb :'users/edit'
 
+  end
 
+  post "/users" do
 
+    user = Users.new
+    user.firstName = params[:firstName]
+    user.lastName = params[:lastName]
+    user.email = params[:email]
+    user.password = params[:password]
+    user.cohortId = Cohorts.get_id(params[:cohortName])
+    user.roleId = Role.get_id(params[:roleName])
 
+    user.save
 
+    redirect "/users"
 
+  end
 
+  put "/users/:id" do
 
+    id = params[:id].to_i
+    user = Users.find(id)
+    user.firstName = params[:firstName]
+    user.lastName = params[:lastName]
+    user.email = params[:email]
+    user.password = params[:password]
+    user.cohortId = Cohorts.get_id(params[:cohortName])
+    user.roleId = Role.get_id(params[:roleName])
 
+    user.save
 
+    redirect "/users"
 
+  end
 
- 
+  delete "/users/:id" do
 
+    id = params[:id].to_i
 
+    Users.destroy(id)
 
+    redirect "users/"
 
- 
-
-
-
-
- 
-
-
-
-
- 
-
-
-  get "/users/:id" do
-
-
- 
-
-
-    id = params[:id].to_i
-
-
- 
-
-
-    @user = Users.find(id)
-
-    @cohorts = Cohorts.all
-
-    @roles = Roles.all
-
-
- 
-
-
-    erb :'users/show'
-
-
- 
-
-
-  end
-
-
- 
-
-
-  get "/users/:id/edit" do
-
-
- 
-
-
-    id = params[:id].to_i
-
-
- 
-
-
-    @user = Users.find(id)
-
-
- 
-
-
-    erb :'users/edit'
-
-
- 
-
-
-  end
-
-
- 
-
-
-  post "/users" do
-
-
- 
-
-
-    user = Users.new
-
-
- 
-
-
-    user.firstName = params[:firstName]
-
-    user.lastName = params[:lastName]
-
-    user.email = params[:email]
-
-    user.password = params[:password]
-
-    user.cohortId = Cohorts.get_id(params[:cohortName])
-
-    user.roleId = Role.get_id(params[:roleName])
-
-
- 
-
-
-    user.save
-
-
- 
-
-
-    redirect "/users"
-
-
- 
-
-
-  end
-
-
- 
-
-
-  put "/users/:id" do
-
-
- 
-
-
-    id = params[:id].to_i
-
-
- 
-
-
-    user = Users.find(id)
-
-
- 
-
-
-    user.firstName = params[:firstName]
-
-    user.lastName = params[:lastName]
-
-    user.email = params[:email]
-
-    user.password = params[:password]
-
-    user.cohortId = Cohorts.get_id(params[:cohortName])
-
-    user.roleId = Role.get_id(params[:roleName])
-
-
- 
-
-
-    user.save
-
-
- 
-
-
-    redirect "/users"
-
-
- 
-
-
-  end
-
-
- 
-
-
-  delete "/users/:id" do
-
-
- 
-
-
-    id = params[:id].to_i
-
-
- 
-
-
-    Users.destroy(id)
-
-
- 
-
-
-    redirect "users/"
-
-
- 
-
-
-  end
-
-
- 
-
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+
+
 
  
