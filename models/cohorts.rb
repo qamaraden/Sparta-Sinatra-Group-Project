163fa8f0f054fc5e2ea3987fee35719edb426bba
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2019-05-08 14:40:45 +0100

    updates

--- models/cohorts.rb
+++ models/cohorts.rb
@@ -2,38 +2,41 @@ require 'pg'
 
 class Cohorts
 
-  attr_accessor(:cohert_id, :cohert_name, :spec_id)
+  attr_accessor(:cohort_id, :cohort_name, :spec_id)
 
   def self.open_connection
     connection = PG.connect(dbname: 'sparta_db')
   end
 
 
-  def self.find(cohert_id)
+  def self.find(cohort_id)
     connection = self.open_connection
 
-    sql = "SELECT cohert_name, specName FROM sparta_view WHERE cohert_id = #{cohert_id} LIMIT 1"
+    sql = "SELECT cohort_name, spec_name FROM sparta_view WHERE cohort_id = #{cohort_id} LIMIT 1"
     cohort = connection.exec(sql)
     cohort = self.hydrate(cohort[0])
     cohort
   end
 
 
+  # sql = "INSERT INTO Cohorts (cohort_name) VALUES ( SELECT #{cohort_name}"
+
   def self.all
     connection = self.open_connection
 
-    sql = "SELECT cohert_name FROM sparta_view GROUP BY cohert_name"
+    sql = "SELECT cohort_name FROM sparta_view GROUP BY cohort_name"
     results = connection.exec(sql)
     cohorts = results.map do |cohort|
     self.hydrate(cohort)
   end
 
 
+
   def self.hydrate(cohort_data)
     cohort = Cohorts.new
 
-    cohort.cohert_id = cohort_data[:cohert_id]
-    cohort.cohert_name = cohort_data[:cohert_name]
+    cohort.cohort_id = cohort_data[:cohort_id]
+    cohort.cohort_name = cohort_data[:cohort_name]
     cohort.spec_id = Specs.get_id(cohort_data[:spec_id])
     cohort
   end
@@ -42,18 +45,19 @@ class Cohorts
   def save
     connection = Cohorts.open_connection
 
-    if (!self.cohert_id)
-      sql = "INSERT INTO cohorts(cohert_name, spec_id) VALUES ('#{self.cohert_name}', '#{self.spec_id}')"
+    if (!self.cohort_id)
+
+      sql = "INSERT INTO cohorts(cohort_name, spec_id) VALUES ('#{self.cohort_name}', '#{self.spec_id}')"
     else
-      sql = "UPDATE cohorts SET cohert_name='#{self.cohert_name}', spec_id='#{self.spec_id}' WHERE id='#{self.cohert_id}'"
+      sql = "UPDATE cohorts SET cohort_name='#{self.cohort_name}', spec_id='#{self.spec_id}' WHERE id='#{self.cohort_id}'"
     end
     connection.exec(sql)
   end
 
 
-  def self.destroy(cohert_id)
+  def self.destroy(cohort_id)
     connection = self.open_connection
-    sql = "DELETE FROM cohorts WHERE cohert_id = #{cohert_id}"
+    sql = "DELETE FROM cohorts WHERE cohort_id = #{cohort_id}"
     connection.exec(sql)
   end
 
