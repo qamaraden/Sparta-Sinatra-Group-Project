7de791920266676be1fbbc79dbb845122e9b2776
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-05-08 10:14:54 +0100

    get rest routes complete

--- controllers/cohorts_controller.rb
+++ controllers/cohorts_controller.rb
@@ -7,4 +7,46 @@ class CohortsController < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  get "/cohorts" do
+
+    @title = "Cohorts"
+
+    @cohorts = Cohorts.all
+
+    erb :'cohorts/index'
+
+  end
+
+  get "/cohorts/new" do
+
+    @cohort = Cohorts.new
+    @specs = Spec.all
+
+    erb :'cohorts/new'
+
+  end
+
+  get "cohorts/:id" do
+
+    id = params[:id].to_i
+
+    @cohort = Cohorts.find(id)
+
+    erb :'cohorts/show'
+
+  end
+
+  get "cohorts/:id/edit" do
+
+    id = params[:id].to_i
+
+    @cohort = Cohorts.find(id)
+
+    erb :'cohorts/edit'
+
+  end
+
+  
+
+
 end
