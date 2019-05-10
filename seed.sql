338bcbfdd242f0a98baf54143a03d59589ff219c
Tech-A30 <tech-a30@Tech-A30s-MacBook-Air.local>
2019-05-10 09:28:04 +0100

    everything

--- seed.sql
+++ seed.sql
@@ -58,13 +58,13 @@ CREATE TABLE users (
   FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
+INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test1', 'lasttest1', 'test1@spartaglobal.com', 'jgnw83@483', 1, 1);
+INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test2', 'lasttest2', 'test2@spartaglobal.com', 'jgnw83@483', 1, 2);
+INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test3', 'lasttest3', 'test3@spartaglobal.com', 'jgnw83@483', 2, 2);
+INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test4', 'lasttest4', 'test4@spartaglobal.com', 'jgnw83@483', 2, 3);
+INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test5', 'lasttest5', 'test5@spartaglobal.com', 'jgnw83@483', 3, 3);
 
-<<<<<<< HEAD
-INSERT INTO users (firstName, lastName, email, password, cohortId, roleId) VALUES ('test1', 'lasttest1', 'test1@spartaglobal.com', 'jgnw83@483', 1, 1);
-INSERT INTO users (firstName, lastName, email, password, cohortId, roleId) VALUES ('test2', 'lasttest2', 'test2@spartaglobal.com', 'jgnw83@483', 1, 2);
-INSERT INTO users (firstName, lastName, email, password, cohortId, roleId) VALUES ('test3', 'lasttest3', 'test3@spartaglobal.com', 'jgnw83@483', 2, 2);
-INSERT INTO users (firstName, lastName, email, password, cohortId, roleId) VALUES ('test4', 'lasttest4', 'test4@spartaglobal.com', 'jgnw83@483', 2, 3);
-INSERT INTO users (firstName, lastName, email, password, cohortId, roleId) VALUES ('test5', 'lasttest5', 'test5@spartaglobal.com', 'jgnw83@483', 3, 3);
+
 
 DROP TABLE IF EXISTS password;
 
@@ -73,13 +73,8 @@ CREATE TABLE password (
   email VARCHAR(255),
   password_salt VARCHAR(255),
   password_hash VARCHAR(255)
-)
-=======
-INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test1', 'lasttest1', 'test1@spartaglobal.com', 'jgnw83@483', 1, 1);
-INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test2', 'lasttest2', 'test2@spartaglobal.com', 'jgnw83@483', 1, 2);
-INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test3', 'lasttest3', 'test3@spartaglobal.com', 'jgnw83@483', 2, 2);
-INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test4', 'lasttest4', 'test4@spartaglobal.com', 'jgnw83@483', 2, 3);
-INSERT INTO users (first_name, last_name, email, password, cohort_id, role_id) VALUES ('test5', 'lasttest5', 'test5@spartaglobal.com', 'jgnw83@483', 3, 3);
+);
+
 
 
 --  VIEW WITH ALL DISPALY INFORMATION
@@ -88,4 +83,3 @@ DROP TABLE IF EXISTS sparta_view;
 
 CREATE VIEW sparta_view AS SELECT users.user_id, users.first_name, users.last_name, users.email, users.password, cohorts.cohort_id, cohorts.cohort_name, roles.role_id, roles.role_name, spec.spec_id, spec.spec_name
 FROM roles INNER JOIN users ON roles.role_id=users.role_id INNER JOIN cohorts ON cohorts.cohort_id=users.cohort_id INNER JOIN spec ON cohorts.spec_id = spec.spec_id ORDER BY users.first_name;
->>>>>>> 2d195abcc3b7be7690c6de76191340cfe69a7287
