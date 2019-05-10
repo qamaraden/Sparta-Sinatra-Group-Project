b7650a501b690d223ad3c3aa44d590027d60352c
swoerkom <swoerkom@spartaglobal.com>
2019-05-10 14:35:03 +0100

    Added auth to route

--- controllers/specs_controller.rb
+++ controllers/specs_controller.rb
@@ -21,69 +21,49 @@ class SpecsController < Sinatra::Base
       erb :'specs/index'
   end
 
-  get "/specs/new"do
-
+  get "/specs/new", :auth => true do
     @spec = Specs.new
-
     erb :'specs/new'
-
   end
 
   get "/specs/:id", :auth => true do
-
     spec_id = params[:id].to_i
-
     @spec = Specs.find(spec_id)
-
     erb :'specs/show'
-
   end
 
   get "/specs/:id/edit", :auth => true do
-
     spec_id = params[:id].to_i
-
-
     @spec = Specs.find(spec_id)
-
     erb :'specs/edit'
-
   end
 
   post "/specs/", :auth => true do
     spec = Specs.new
     spec.spec_name = params[:spec_name]
     spec.save
-
     redirect "/specs"
-
   end
 
   put "/specs/:id", :auth => true do
-
     spec_id = params[:id].to_i
     spec = Specs.find(spec_id)
     spec.spec_name = params[:spec_name]
     spec.save
-
     redirect "/specs"
-
   end
 
   delete "/specs/:id", :auth => true do
-
     id = params[:id].to_i
     @check = Specs.check_id(id)
-
     if (@check == 0)
       Specs.destroy(id)
-
       redirect "/specs"
     else
       @error_message = "Error, Specialisation in use."
       @spec = Specs.find(id)
       erb :"specs/show"
     end
-
   end
+  
 end
